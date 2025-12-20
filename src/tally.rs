a83879ecd4fff58d0feee6377aa0944069e893b4
Sam Pritchard <sampritchard0801@gmail.com>
2025-12-20 01:11:55 +0000

    🎨 style(tally): Link the setup URL instead.

--- src/tally.rs
+++ src/tally.rs
@@ -8,33 +8,19 @@ use urlencoding::encode;
 pub async fn tally(
     ctx: Context<'_>,
     #[description = "The name of the tallylight.io room."] room: String,
-    #[description = "The port OBS WebSocket Server is listening on."]
-    #[min = 1024]
-    #[max = 65535]
-    port: Option<u16>,
-    #[description = "The password for the OBS WebSocket Server."] password: Option<String>,
 ) -> Result<(), Error> {
-    let port = &port.unwrap_or(4455).to_string();
-    let password = &password.unwrap_or(String::new().to_string());
-
-    let transmitter_url = format!(
-        "https://v2.tallylight.io/room/{}/transmit/obs?obsPort={}&obsPassword={}&mode=obs",
-        encode(&room),
-        encode(port),
-        encode(password)
-    );
-
+    let tally_setup_url = format!("https://v2.tallylight.io/room/{}/setup/obs", encode(&room));
     let tally_url = format!("https://v2.tallylight.io/room/{}/view", encode(&room));
 
     let embed = CreateEmbed::new()
         .title("Tally Light Links")
-        .description("Add the transmitter URL to OBS and give the viewer URL to your observers.")
+        .description("Click the setup URL to add the transmitter. The Viewer URL is for observers.")
         .field(
-            "Transmitter URL",
-            format!("```{}```", transmitter_url),
-            false,
+            "Setup URL",
+            format!("[Click Here]({})", tally_setup_url),
+            true,
         )
-        .field("Viewer URL", format!("```{}```", tally_url), false);
+        .field("Viewer URL", format!("```{}```", tally_url), true);
 
     let reply = CreateReply::default().embed(embed);
     ctx.send(reply).await?;
