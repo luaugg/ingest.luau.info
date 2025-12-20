c0ccfef3c91b9aa9fc99820fe14b85563f6ff5bb
Sam Pritchard <sampritchard0801@gmail.com>
2025-12-20 02:19:11 +0000

    🎨 style(srt): Formatting run on Cloudflare API wrapper.

--- src/cloudflare.rs
+++ src/cloudflare.rs
@@ -1,14 +1,13 @@
 use cloudflare::{
-    endpoints::{
-        dns::dns::{ListDnsRecords, ListDnsRecordsParams, UpdateDnsRecord, UpdateDnsRecordParams, DnsRecord},
+    endpoints::dns::dns::{
+        DnsRecord, ListDnsRecords, ListDnsRecordsParams, UpdateDnsRecord, UpdateDnsRecordParams,
     },
     framework::{
+        Environment, Error as CloudflareError,
         auth::Credentials,
-        response::{ApiSuccess, ApiFailure},
-        client::{async_api::Client, ClientConfig},
-        Environment,
-        Error as CloudflareError,
-    }
+        client::{ClientConfig, async_api::Client},
+        response::{ApiFailure, ApiSuccess},
+    },
 };
 
 type ApiResult<T> = Result<ApiSuccess<T>, ApiFailure>;
@@ -18,12 +17,28 @@ pub async fn get_cloudflare_client(token: String) -> Result<Client, CloudflareEr
     Client::new(creds, ClientConfig::default(), Environment::Production)
 }
 
-pub async fn list_dns_records(client: &Client, zone_identifier: String, params: ListDnsRecordsParams) -> ApiResult<Vec<DnsRecord>> {
-    let request = ListDnsRecords { zone_identifier: &zone_identifier, params };
+pub async fn list_dns_records(
+    client: &Client,
+    zone_identifier: String,
+    params: ListDnsRecordsParams,
+) -> ApiResult<Vec<DnsRecord>> {
+    let request = ListDnsRecords {
+        zone_identifier: &zone_identifier,
+        params,
+    };
     client.request(&request).await
 }
 
-pub async fn update_dns_record(client: &Client, zone_identifier: String, identifier: String, params: UpdateDnsRecordParams<'_>) -> ApiResult<DnsRecord> {
-    let request = UpdateDnsRecord { zone_identifier: &zone_identifier, identifier: &identifier, params };
+pub async fn update_dns_record(
+    client: &Client,
+    zone_identifier: String,
+    identifier: String,
+    params: UpdateDnsRecordParams<'_>,
+) -> ApiResult<DnsRecord> {
+    let request = UpdateDnsRecord {
+        zone_identifier: &zone_identifier,
+        identifier: &identifier,
+        params,
+    };
     client.request(&request).await
 }
