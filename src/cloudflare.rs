905c7856833e7f94f079e1c6ed50bfbdc67e3075
Sam Pritchard <sampritchard0801@gmail.com>
2025-12-20 02:17:18 +0000

    🐛 fix(srt): Fix errors in Cloudflare API wrapper.

--- src/cloudflare.rs
+++ src/cloudflare.rs
@@ -1,29 +1,29 @@
 use cloudflare::{
     endpoints::{
-        dns::dns::{ListDnsRecords, ListDnsRecordsParams, UpdateDnsRecord, UpdateDnsRecordParams, DnsRecord, DnsContent},
+        dns::dns::{ListDnsRecords, ListDnsRecordsParams, UpdateDnsRecord, UpdateDnsRecordParams, DnsRecord},
     },
     framework::{
         auth::Credentials,
-        response::{ApiFailure, ApiResponse},
+        response::{ApiSuccess, ApiFailure},
         client::{async_api::Client, ClientConfig},
         Environment,
         Error as CloudflareError,
     }
 };
 
-type Result<T> = std::result::Result<T, CloudflareError>;
+type ApiResult<T> = Result<ApiSuccess<T>, ApiFailure>;
 
-pub async fn get_cloudflare_client(token: String) -> Result<Client> {
+pub async fn get_cloudflare_client(token: String) -> Result<Client, CloudflareError> {
     let creds = Credentials::UserAuthToken { token };
     Client::new(creds, ClientConfig::default(), Environment::Production)
 }
 
-pub async fn list_dns_records(client: &Client, zone_identifier: String, params: ListDnsRecordsParams) -> Result<ApiResponse<Vec<DnsRecord>>> {
+pub async fn list_dns_records(client: &Client, zone_identifier: String, params: ListDnsRecordsParams) -> ApiResult<Vec<DnsRecord>> {
     let request = ListDnsRecords { zone_identifier: &zone_identifier, params };
     client.request(&request).await
 }
 
-pub async fn update_dns_record(client: &Client, zone_identifier: String, identifier: String, params: UpdateDnsRecordParams) -> Result<ApiResponse<DnsRecord>> {
+pub async fn update_dns_record(client: &Client, zone_identifier: String, identifier: String, params: UpdateDnsRecordParams<'_>) -> ApiResult<DnsRecord> {
     let request = UpdateDnsRecord { zone_identifier: &zone_identifier, identifier: &identifier, params };
     client.request(&request).await
 }
