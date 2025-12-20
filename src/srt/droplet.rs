8dbbb8ec7872fa6278e9f7c6d1b477db59e921c5
Sam Pritchard <sampritchard0801@gmail.com>
2025-12-20 22:18:49 +0000

    ✨ feature(srt): Add in the droplet commands.

--- src/srt/droplet.rs
+++ src/srt/droplet.rs
@@ -1,33 +1,54 @@
 use crate::{Context, Error};
 
+use digitalocean_api::prelude::*;
 use poise::CreateReply;
 use serenity::all::CreateEmbed;
-use urlencoding::encode;
 
-#[poise::command(
-    slash_command,
-    subcommands("create", "delete", "status"),
-    subcommand_required
-)]
+#[poise::command(slash_command, subcommands("create", "delete"), subcommand_required)]
 pub async fn droplet(_ctx: Context<'_>) -> Result<(), Error> {
     // Parent stub of the droplet commands; this will never execute.
     Ok(())
 }
 
-#[poise::command(slash_command)]
-pub async fn create(_ctx: Context<'_>) -> Result<(), Error> {
-    // Implementation for creating a droplet
+#[poise::command(slash_command, owners_only)]
+pub async fn create(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.defer_ephemeral().await?;
+    let snapshot_id = std::env::var("DIGITALOCEAN_SNAPSHOT_ID")
+        .expect("DIGITALOCEAN_SNAPSHOT_ID environment variable not set");
+
+    Droplet::create("ingest", "lon1", "c-4", snapshot_id)
+        .execute(&ctx.data().digitalocean_client)
+        .await?;
+
+    let embed = CreateEmbed::default()
+        .title("Droplet Created")
+        .description("You should follow up this command with `/dns update`.");
+    let reply = CreateReply::default().embed(embed);
+
+    ctx.send(reply).await?;
     Ok(())
 }
 
-#[poise::command(slash_command)]
-pub async fn delete(_ctx: Context<'_>) -> Result<(), Error> {
-    // Implementation for deleting a droplet
-    Ok(())
-}
+#[poise::command(slash_command, owners_only)]
+pub async fn delete(ctx: Context<'_>) -> Result<(), Error> {
+    ctx.defer_ephemeral().await?;
 
-#[poise::command(slash_command)]
-pub async fn status(_ctx: Context<'_>) -> Result<(), Error> {
-    // Implementation for getting status of a droplet
+    let mut embed = CreateEmbed::default();
+    let droplet = Droplet::list()
+        .execute(&ctx.data().digitalocean_client)
+        .await?;
+    let matching = droplet.iter().find(|droplet| droplet.name() == "ingest");
+
+    if let Some(droplet) = matching {
+        Droplet::delete(*droplet.id())
+            .execute(&ctx.data().digitalocean_client)
+            .await?;
+        embed = embed.description("The droplet has been deleted.")
+    } else {
+        embed = embed.description("No droplet found with the name 'ingest'.")
+    }
+
+    let reply = CreateReply::default().embed(embed);
+    ctx.send(reply).await?;
     Ok(())
 }
