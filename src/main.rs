48796b787b9344c76b4ec991cead747466bfc4fd
Sam Pritchard <sampritchard0801@gmail.com>
2025-12-20 02:56:21 +0000

    🚧 wip: add cloudflare and digitalocean clients to data struct

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,36 @@
+mod cloudflare;
+mod srt;
 mod syncer;
 mod tally;
 
-use std::env;
-
+use ::cloudflare::framework::client::async_api::Client as CloudflareClient;
+use digitalocean_api::prelude::DigitalOcean;
 use poise::serenity_prelude as serenity;
+use std::env;
 
 type Error = Box<dyn std::error::Error + Send + Sync>;
 type Context<'a> = poise::Context<'a, Data, Error>;
-pub struct Data {}
+
+pub struct Data {
+    cloudflare_client: CloudflareClient,
+    digitalocean_client: DigitalOcean,
+}
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
+
+    let discord_token = env::var("DISCORD_BOT_TOKEN").expect("missing DISCORD_BOT_TOKEN env var");
+    let cloudflare_token =
+        env::var("CLOUDFLARE_API_TOKEN").expect("missing CLOUDFLARE_API_TOKEN env var");
+    let digitalocean_token =
+        env::var("DIGITALOCEAN_API_TOKEN").expect("missing DIGITALOCEAN_API_TOKEN env var");
+    let data = Data {
+        cloudflare_client: cloudflare::get_cloudflare_client(cloudflare_token)
+            .await
+            .expect("failed to create cloudflare client"),
+        digitalocean_client: DigitalOcean::new(digitalocean_token).unwrap(),
+    };
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions {
@@ -31,15 +50,14 @@ async fn main() {
         .setup(move |ctx, _ready, framework| {
             Box::pin(async move {
                 poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                Ok(Data {})
+                Ok(data)
             })
         })
         .build();
 
-    let token = env::var("DISCORD_BOT_TOKEN").expect("missing DISCORD_BOT_TOKEN env var");
     let intents =
         serenity::GatewayIntents::non_privileged() | serenity::GatewayIntents::MESSAGE_CONTENT;
-    let mut client = serenity::ClientBuilder::new(token, intents)
+    let mut client = serenity::ClientBuilder::new(discord_token, intents)
         .framework(framework)
         .await
         .expect("error creating bot client");
