175c1f6bdec94b5c0560c66655d1d76468a56671
Sam Pritchard <sampritchard0801@gmail.com>
2026-02-15 15:14:45 +0000

    rework droplet command

--- src/srt/droplet.rs
+++ src/srt/droplet.rs
@@ -16,17 +16,33 @@ pub async fn droplet(_ctx: Context<'_>) -> Result<(), Error> {
 #[poise::command(slash_command, owners_only)]
 pub async fn create(ctx: Context<'_>) -> Result<(), Error> {
     ctx.defer().await?;
-    let description = match ctx.data().digitalocean_client.create_droplet().await {
-        Ok(_) => {
-            sleep(Duration::from_secs(45)).await; // not guaranteed to be done by now but we're fine with that.
-            "Droplet created. Follow up this command with `/srt dns update`.".to_owned()
-        }
-        Err(err) => format!("Failed to create droplet: {}", err),
-    };
 
-    let embed = CreateEmbed::default().description(description);
-    let reply = CreateReply::default().embed(embed);
-    ctx.send(reply).await?;
+    match ctx.data().digitalocean_client.create_droplet().await {
+        Ok(droplet) => {
+            let embed = CreateEmbed::default().description(format!(
+                "Droplet created with ID `{}`. Wait a minute for it to be ready.",
+                droplet.id()
+            ));
+
+            let reply_builder = CreateReply::default().embed(embed.clone());
+            let reply = ctx.send(reply_builder.clone()).await?;
+            sleep(Duration::from_secs(45)).await;
+
+            reply
+                .edit(
+                    ctx,
+                    reply_builder.embed(embed.description("Droplet is now ready.")),
+                )
+                .await?;
+        }
+        Err(err) => {
+            let error_message = format!("Failed to create droplet: {}", err);
+            let embed = CreateEmbed::default().description(error_message);
+            let reply = CreateReply::default().embed(embed);
+            ctx.send(reply).await?;
+        }
+    }
+
     Ok(())
 }
 
