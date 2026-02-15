58b3f7dbbcd36018d5e12afa24697a9a0eb11cb3
Sam Pritchard <sampritchard0801@gmail.com>
2026-02-15 15:49:49 +0000

    clean up syncer code

--- src/syncer.rs
+++ src/syncer.rs
@@ -2,7 +2,6 @@ use crate::{Context, Error};
 
 use poise::CreateReply;
 use serenity::all::CreateEmbed;
-use urlencoding::encode;
 
 /// Generates syncer links with the provided options. \
 /// Remember to press Show on the producer link.
@@ -16,58 +15,32 @@ pub async fn syncer(
     stream: &'static str,
     #[description = "Whether to show the system clock offset."] offset: Option<bool>,
     #[description = "Whether to center the syncer on the webpage."] center: Option<bool>,
-    #[description = "Remote group ID of the syncer (so you can hide it later)."] group_id: Option<
-        String,
-    >,
-    #[description = "Whether to split the syncer progress bar into segments."] split: Option<bool>,
-    #[description = "The timezone to display the syncer in."] timezone: Option<String>,
-    #[description = "Whether to enable extra precision on the second counter."]
-    centiseconds: Option<bool>,
-    #[description = "Whether to enable an audible beep each second."] beep: Option<bool>,
-    #[description = "The frequency of the beep sound in Hz."]
-    #[min = 200_u32]
-    #[max = 1000_u32]
-    frequency: Option<u32>,
 ) -> Result<(), Error> {
     let mut url = "```https://syncer.live/embed?".to_owned();
+    let mut producer_url = "```https://syncer.live/control?groupId=".to_owned();
 
     match stream {
-        "USS A Stream" => url.push_str("&label=USS%20A%20Stream"),
-        "USS B Stream" => url.push_str("&label=USS%20B%20Stream"),
+        "USS A Stream" => {
+            url.push_str("&label=USS%20A%20Stream");
+            url.push_str("&groupId=uss-2026-spring-a");
+            producer_url.push_str("uss-2026-spring-a```");
+        }
+        "USS B Stream" => {
+            url.push_str("&label=USS%20B%20Stream");
+            url.push_str("&groupId=uss-2026-spring-b");
+            producer_url.push_str("uss-2026-spring-b```");
+        }
         _ => (),
     }
 
     url.push_str(&format!("&offset={}", offset.unwrap_or(true)));
-    url.push_str(&format!("&center={}", center.unwrap_or(true)));
-    url.push_str(&format!(
-        "&groupId={}",
-        encode(&(&group_id).clone().unwrap_or(String::from("")))
-    ));
-    url.push_str(&format!("&split={}", split.unwrap_or(false)));
-    url.push_str(&format!(
-        "&timezone={}",
-        encode(&timezone.unwrap_or(String::from("Etc/UTC")))
-    ));
-    url.push_str(&format!("&centiseconds={}", centiseconds.unwrap_or(false)));
-    url.push_str(&format!("&beep={}", beep.unwrap_or(false)));
-    url.push_str(&format!(
-        "&beepFrequency={}```",
-        frequency.unwrap_or(440_u32)
-    ));
+    url.push_str(&format!("&center={}```", center.unwrap_or(true)));
 
-    let mut embed = CreateEmbed::default()
+    let embed = CreateEmbed::default()
         .title("Syncer Links")
-        .field("Observer", url, false);
-
-    if let Some(group_id) = group_id {
-        let producer_url = format!(
-            "```https://syncer.live/control?groupId={}```",
-            encode(&group_id)
-        );
-        embed = embed
-            .description("If this is a new syncer, click **Show** on the Producer link to show it.")
-            .field("Producer", producer_url, false);
-    }
+        .description("The **Producer** link is to control the syncer visibility.")
+        .field("Observer", url, false)
+        .field("Producer", producer_url, false);
 
     let reply = CreateReply::default().embed(embed);
     ctx.send(reply).await?;
